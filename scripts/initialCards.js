e17e7c23545e6b5acd52a4509353c6434ef3bb76
Nitzan Cohen <nitzancohen@hotmail.com>
2021-10-08 00:51:11 +0300

    implement css rules for all elements

--- scripts/initialCards.js
+++ scripts/initialCards.js
@@ -1,7 +1,9 @@
-export const initialCards = [{
+export const initialCards = [
+  {
     imageSrc: "./images/card-mila.png",
     imageAlt: "Mila Gramm",
     title: "Mila Gramm",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
     contentTag: ["Design", "Sport", "Music"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
@@ -10,6 +12,7 @@ export const initialCards = [{
     imageSrc: "./images/card-robert.png",
     imageAlt: "Robert Fox",
     title: "Robert Fox",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
     contentTag: ["Languages", "Football", "Traveling", "Animals"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
@@ -18,6 +21,7 @@ export const initialCards = [{
     imageSrc: "./images/card-bessie.png",
     imageAlt: "Bessie Cooper",
     title: "Bessie Cooper",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
     contentTag: ["Psychology", "Science", "Music", "Coding"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
@@ -26,6 +30,7 @@ export const initialCards = [{
     imageSrc: "./images/card-cameron.png",
     imageAlt: "Cameron Williamson",
     title: "Cameron Williamson",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
     contentTag: ["Sport", "Music", "Astrology", "Plants"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
@@ -34,6 +39,7 @@ export const initialCards = [{
     imageSrc: "./images/card-jenny.png",
     imageAlt: "Jenny Wilson",
     title: "Jenny Wilson",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
     contentTag: ["VR Reality", "Gaming", "Sport", "Travelling"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
@@ -42,80 +48,144 @@ export const initialCards = [{
     imageSrc: "./images/card-leslie.png",
     imageAlt: "Leslie Alexander",
     title: "Leslie Alexander",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
     contentTag: ["Animals", "Coding", "Researches"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
   },
   {
-    imageSrc: "./images/image.svg",
-    imageAlt: "Profile picture",
-    title: "User Name",
+    imageSrc: "./images/card-esther.png",
+    imageAlt: "Esther Howard",
+    title: "Esther Howard",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
-    contentTag: ["interest", "interest", "interest", "interest"],
+    contentTag: ["Coding", "Marketing", "Design", "Dancing"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
   },
   {
-    imageSrc: "./images/image.svg",
-    imageAlt: "Profile picture",
-    title: "User Name",
+    imageSrc: "./images/card-brooklyn.png",
+    imageAlt: "Brooklyn Simmons",
+    title: "Brooklyn Simmons",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
-    contentTag: ["interest", "interest", "interest", "interest"],
+    contentTag: ["Marketing", "Social media", "Videomaking"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
   },
   {
-    imageSrc: "./images/image.svg",
-    imageAlt: "Profile picture",
-    title: "User Name",
+    imageSrc: "./images/card-wade.png",
+    imageAlt: "Wade Warren",
+    title: "Wade Warren",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
-    contentTag: ["interest", "interest", "interest", "interest"],
+    contentTag: ["Sport", "Art.", "Marketing"],
+    description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
+  },
+  {  
+  imageSrc: "./images/card-jenny.png",
+  imageAlt: "Savannah Nguyen",
+  title: "Savannah Nguyen",
+  city: "New York",
+  subtitle: "Student at Yandex Praktikum",
+  contentTag: ["VR Reality", "Gaming", "Sport", "Travelling"],
+  description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
+  },
+  {
+    imageSrc: "./images/card-leslie.png",
+    imageAlt: "Cody Fisher",
+    title: "Cody Fisher",
+    city: "New York",
+    subtitle: "Student at Yandex Praktikum",
+    contentTag: ["Animals", "Coding", "Researches"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
   },
   {
-    imageSrc: "./images/image.svg",
-    imageAlt: "Profile picture",
-    title: "User Name",
+    imageSrc: "./images/card-anette.png",
+    imageAlt: "Annette Black",
+    title: "Annette Black",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
-    contentTag: ["interest", "interest", "interest", "interest"],
+    contentTag: ["Animals", "Coding", "Researches"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
   },
   {
-    imageSrc: "./images/image.svg",
-    imageAlt: "Profile picture",
-    title: "User Name",
+    imageSrc: "./images/card-brooklyn.png",
+    imageAlt: "Albert Flores",
+    title: "Albert Flores",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
-    contentTag: ["interest", "interest", "interest", "interest"],
+    contentTag: ["Marketing", "Social media", "Videomaking"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
   },
   {
-    imageSrc: "./images/image.svg",
-    imageAlt: "Profile picture",
-    title: "User Name",
+    imageSrc: "./images/card-devon.png",
+    imageAlt: "Devon Lane",
+    title: "Devon Lane",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
-    contentTag: ["interest", "interest", "interest", "interest"],
+    contentTag: ["Marketing", "Social media", "Videomaking"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
   },
   {
-    imageSrc: "./images/image.svg",
-    imageAlt: "Profile picture",
-    title: "User Name",
+    imageSrc: "./images/card-ronald.png",
+    imageAlt: "Ronald Richard",
+    title: "Ronald Richard",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
-    contentTag: ["interest", "interest", "interest", "interest"],
+    contentTag: ["Marketing", "Social media", "Videomaking"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
   },
   {
-    imageSrc: "./images/image.svg",
-    imageAlt: "Profile picture",
-    title: "User Name",
+    imageSrc: "./images/card-darlene.png",
+    imageAlt: "Darlene Robertson",
+    title: "Darlene Robertson",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
-    contentTag: ["interest", "interest", "interest", "interest"],
+    contentTag: ["Marketing", "Social media", "Videomaking"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
   },
   {
-    imageSrc: "./images/image.svg",
-    imageAlt: "Profile picture",
-    title: "User Name",
+    imageSrc: "./images/card-dianne.png",
+    imageAlt: "Dianne Russell",
+    title: "Dianne Russell",
+    city: "New York",
     subtitle: "Student at Yandex Praktikum",
-    contentTag: ["interest", "interest", "interest", "interest"],
+    contentTag: ["Marketing", "Social media", "Videomaking"],
     description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
   },
+  {
+    imageSrc: "./images/card-mila.png",
+    imageAlt: "Jerome Bell",
+    title: "Jerome Bell",
+    city: "New York",
+    subtitle: "Student at Yandex Praktikum",
+    contentTag: ["Marketing", "Social media", "Videomaking"],
+    description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
+  },
+  {
+    imageSrc: "./images/card-esther.png",
+    imageAlt: "Arlene McCoy",
+    title: "Arlene McCoy",
+    city: "New York",
+    subtitle: "Student at Yandex Praktikum",
+    contentTag: ["Coding", "Marketing", "Design", "Dancing"],
+    description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
+  },
+  {
+    imageSrc: "./images/card-cameron.png",
+    imageAlt: "Floyd Miles",
+    title: "Floyd Miles",
+    city: "New York",
+    subtitle: "Student at Yandex Praktikum",
+    contentTag: ["Sport", "Music", "Astrology", "Plants"],
+    description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
+  },
+  {
+    imageSrc: "./images/card-robert.png",
+    imageAlt: "Marvin McKinney",
+    title: "Marvin McKinney",
+    city: "New York",
+    subtitle: "Student at Yandex Praktikum",
+    contentTag: ["Languages", "Football", "Traveling", "Animals"],
+    description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint."
+  }
 ]
